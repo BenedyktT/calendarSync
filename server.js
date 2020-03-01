7994800a0caeb2d76fb901e6f7b034fdb28a49df
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-03-01 19:43:47 +0000

    octal iteral bugfix

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
 	wakeUp({
 		url: "http://blooming-tundra-68800.herokuapp.com/", // url string
-		interval: 60000 * 25, // interval in milliseconds (1 minute in this example)
+		interval: 60000 * 25,
+		startNap: [21, 30, 21, 12], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
+		endNap: [9, 23, 59, 999] // interval in milliseconds (1 minute in this example)
 	}).start();
 	schedule.scheduleJob("0 */1 * * *", async () => {
 		console.log("done");
