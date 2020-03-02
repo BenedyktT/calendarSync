cf6a3d7194303010f6a9a43891892fe6b72c8f7d
Benedykt <32865753+BenedyktT@users.noreply.github.com>
2020-03-02 08:36:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.listen(PORT, () => {
 		url: "http://blooming-tundra-68800.herokuapp.com/", // url string
 		interval: 60000 * 25,
 		startNap: [21, 30, 21, 12], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
-		endNap: [9, 23, 59, 999] // interval in milliseconds (1 minute in this example)
+		endNap: [7, 11, 59, 999] // interval in milliseconds (1 minute in this example)
 	}).start();
-	schedule.scheduleJob("0 */1 * * *", async () => {
+	schedule.scheduleJob("00 */10 * * * *", async () => {
 		console.log("done");
 		await calendarSync();
 	});
