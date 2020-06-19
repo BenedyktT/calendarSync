06a533a3cb3298fdb43184dc30c422ab78e7cd20
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-06-19 15:25:56 +0000

    change to take prices from booking.com

--- getOccupancy.js
+++ getOccupancy.js
@@ -2,24 +2,20 @@ import axios from "axios";
 import moment from "moment";
 import convert from "xml-js";
 
-const getOutofOrder = async startDate => {
-	const stayFrom = moment(startDate)
-		.subtract(1, "month")
-		.format("YYYY-MM-DD");
-	const stayTo = moment(stayFrom)
-		.add(2, "month")
-		.format("YYYY-MM-DD");
+const getOutofOrder = async (startDate) => {
+	const stayFrom = moment(startDate).subtract(1, "month").format("YYYY-MM-DD");
+	const stayTo = moment(stayFrom).add(2, "month").format("YYYY-MM-DD");
 	try {
 		const response = await axios.get(
 			`services/bookingapi/reservations?stayFromDate=${stayFrom}&stayToDate=${stayTo}&includeOutOfOrder=true&includeInvoices=false&modifiedSince=${moment().format(
 				"YYYY-MM-DDTHH:mm:ss"
 			)}`
 		);
-		const result = response.data.reservations.map(e => ({
+		const result = response.data.reservations.map((e) => ({
 			rooms: e.rooms.length,
 			startDate: e.rooms[0].dateArrival,
 			endDate: e.rooms[0].dateDeparture,
-			room: e.rooms[0].roomNumber
+			room: e.rooms[0].roomNumber,
 		}));
 		return result;
 	} catch (error) {
@@ -31,19 +27,17 @@ const getOutofOrder = async startDate => {
 export default async (req, res) => {
 	try {
 		const today = moment().format("YYYY-MM-DD");
-		const monthFromToday = moment()
-			.add(8, "month")
-			.format("YYYY-MM-DD");
+		const monthFromToday = moment().add(8, "month").format("YYYY-MM-DD");
 		const outOfOrder = await getOutofOrder(today);
 
 		const response = await axios.get(
-			`https://api.roomercloud.net/services/bookingapi/availability1?hotel=LAKI&channelCode=TRAVEL&channelManagerCode=OWN&arrivalDate=${today}&departureDate=${monthFromToday}`
+			`https://api.roomercloud.net/services/bookingapi/availability1?hotel=LAKI&channelCode=HOT&channelManagerCode=OWN&arrivalDate=${today}&departureDate=${monthFromToday}`
 		);
 		const result = convert.xml2js(response.data, {
 			compact: true,
-			spaces: 4
+			spaces: 4,
 		});
-		const month = result.availability.inventory.inventoryItem.map(e => {
+		const month = result.availability.inventory.inventoryItem.map((e) => {
 			const availabilityToOccupancy = (baseCode, availability) => {
 				let occupancy = 0;
 				if (baseCode === "SUP-S") occupancy = 1 - availability;
@@ -57,28 +51,28 @@ export default async (req, res) => {
 				baseCode,
 				description: e._attributes.description,
 				inventoryCode: e._attributes.inventoryCode,
-				availability: e.availabilityAndRates.day.map(x => ({
+				availability: e.availabilityAndRates.day.map((x) => ({
 					date: x._attributes.date,
 					availability: x._attributes.availability,
 					rate: x._attributes.rate,
 					occupancy: availabilityToOccupancy(
 						baseCode,
 						parseInt(x._attributes.availability)
-					)
-				}))
+					),
+				})),
 			};
 		});
 		let total = month
-			.map(category =>
-				category.availability.map(x => ({
+			.map((category) =>
+				category.availability.map((x) => ({
 					...x,
-					baseCode: category.baseCode
+					baseCode: category.baseCode,
 				}))
 			)
 			.reduce((acc, curr) => [...acc, ...curr], [])
 			.reduce((acc, curr) => {
 				const x = acc.find(
-					e => e.baseCode === curr.baseCode && e.date === curr.date
+					(e) => e.baseCode === curr.baseCode && e.date === curr.date
 				);
 				if (!x) {
 					return [...acc, curr];
@@ -86,8 +80,8 @@ export default async (req, res) => {
 				return acc;
 			}, [])
 			.reduce((acc, curr, i, arr) => {
-				const x = arr.filter(e => e.date === curr.date);
-				const y = acc.find(y => y.date === curr.date);
+				const x = arr.filter((e) => e.date === curr.date);
+				const y = acc.find((y) => y.date === curr.date);
 				if (!y) {
 					return [
 						...acc,
@@ -100,16 +94,16 @@ export default async (req, res) => {
 										parseInt(a.rate) < parseInt(c.rate) && parseInt(a.rate) > 0
 											? a.rate
 											: c.rate),
-									occupancy: c.occupancy + a.occupancy
+									occupancy: c.occupancy + a.occupancy,
 								};
 							},
 							{ rate: 0, occupancy: 0 }
-						)
+						),
 					];
 				} else return acc;
 			}, [])
 			.reduce((acc, curr) => {
-				const findDate = outOfOrder.filter(e => {
+				const findDate = outOfOrder.filter((e) => {
 					return (
 						moment(curr.date).isSameOrAfter(e.startDate) &&
 						moment(curr.date).isBefore(e.endDate)
@@ -123,7 +117,7 @@ export default async (req, res) => {
 					);
 					return [
 						...acc,
-						{ ...curr, occupancy: curr.occupancy - totalOccupancy }
+						{ ...curr, occupancy: curr.occupancy - totalOccupancy },
 					];
 				}
 
